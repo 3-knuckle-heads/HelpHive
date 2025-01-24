8c858f10d63ecc511eaa070f9872c533ab557abb
Pabak Dev <53967227+pabak-dev@users.noreply.github.com>
2025-01-24 13:34:25 +0600

    Setup app logo

--- FrontEnd/help-hive/src/App.jsx
+++ FrontEnd/help-hive/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import helphiveLogo from './assets/helphive.png'
 import './App.css'
 
 function App() {
@@ -8,15 +9,16 @@ function App() {
 
   return (
     <>
+    <h2>HelpHive | Volunteer management</h2>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={helphiveLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>CSE 2200</h1>
+      
       <div className="card">
         <button onClick={() => setCount((count) => count + 10)}>
           count is {count}
