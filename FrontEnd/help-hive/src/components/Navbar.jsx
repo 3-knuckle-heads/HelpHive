7185f67801cd61d8fbd058fbb5dde78fa3716dab
Pabak Dev <53967227+pabak-dev@users.noreply.github.com>
2025-01-24 19:12:21 +0600

    Navbar now works for mobile devices

--- FrontEnd/help-hive/src/components/Navbar.jsx
+++ FrontEnd/help-hive/src/components/Navbar.jsx
@@ -3,32 +3,49 @@ import helphiveLogo from "../assets/helphive.png";
 
 function Navbar() {
   return (
-    <header className="shadow mb-2">
-      <div className="max-w-screen-xl mx-auto py-4 flex justify-between items-center">
+    <header className="shadow mb-2 px-4">
+      <div className="relative max-w-screen-lg mx-auto py-4 flex flex-col md:flex-row md:justify-between md:items-center">
         <a className="text-2xl flex items-center" href="#">
           <span className="size-20">
             <img src={helphiveLogo} className="logo" />
           </span>
-          <span class="text-black font-extrabold px-2 mb-3">
-            Volunteer Management Application
+          <span className="text-black font-bold px-2 mb-1 lg:mb-2 text-xl md:text-2xl lg:text-3xl">
+            Volunteer Network
           </span>
         </a>
-        <nav className="">
-          <ul className="flex xl:gap-x-8">
+        <input className="peer hidden" type="checkbox" id="nav-open" />
+        <label className="absolute right-2 mt-6 md:hidden" htmlFor="nav-open">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="currentColor"
+            className="size-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </label>
+        <nav className="peer-checked:block md:block hidden">
+          <ul className="flex flex-col gap-y-4 md:flex-row gap-x-2 lg:gap-x-6">
             <li className="">
-              <a href="#" className="hover:text-orange-300">
+              <a href="#" className="text-gray-600 hover:text-orange-400">
                 Contact
               </a>
             </li>
             <li className="">
-              <a href="#" className="hover:text-orange-300">
+              <a href="#" className="text-gray-600 hover:text-orange-400">
                 About
               </a>
             </li>
-            <li className="">
+            <li className="mt-2 md:mt-0">
               <a
                 href="#"
-                className="border-2 px-4 py-2 rounded-2xl border-orange-400 font-semibold text-orange-400 hover:text-blue-400 hover:border-blue-400"
+                className="border-2 px-4 py-2 rounded-2xl border-orange-500 font-semibold text-orange-500 hover:text-blue-500 hover:border-blue-500"
               >
                 Log in
               </a>
