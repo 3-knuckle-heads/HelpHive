24d034379b0861b8f2674d5a486d86f79bc7d465
Pabak Dev <53967227+pabak-dev@users.noreply.github.com>
2025-01-24 15:00:02 +0600

    imported tailwind css

--- FrontEnd/help-hive/src/App.jsx
+++ FrontEnd/help-hive/src/App.jsx
@@ -1,36 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import helphiveLogo from './assets/helphive.png'
 import './App.css'
+import Home from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <h2>HelpHive | Volunteer management</h2>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={helphiveLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 10)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div><Home/></div>
   )
 }
 
---
+++ FrontEnd/help-hive/src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from '../assets/react.svg'
+import helphiveLogo from '../assets/helphive.png'
+import './Home.css'
+
+function Home() {
+    const [count, setCount] = useState(0)
+
+  return (
+    <div>
+    <h2>HelpHive | Volunteer management</h2>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={helphiveLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 10)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default Home
