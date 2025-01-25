4dca7a73d97bf5a75a126452f321ec6cc8e6494a
Pabak Dev <53967227+pabak-dev@users.noreply.github.com>
2025-01-26 02:31:40 +0600

    Home restructured for better clarity

--- FrontEnd/help-hive/src/pages/Home.jsx
+++ FrontEnd/help-hive/src/pages/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
         backgroundImage: `url(${bgImage})`,
       }}
     >
-      <header className="text-center mb-12 mt-16">
+      <header className="text-center mb-8 mt-16">
         <h1 className="text-5xl font-bold text-blue-500 mb-4">
           Welcome to HelpHive
         </h1>
@@ -22,11 +22,6 @@ function Home() {
         </p>
       </header>
       <main className="text-center">
-        <h2 className="text-3xl font-semibold text-gray-800 mb-6">Why us?</h2>
-        <p className="text-gray-600 mb-8 max-w-xl">
-          If you want to volunteer, host a campaign, or connect with others,
-          HelpHive is your perfect platform.
-        </p>
         <div className="flex flex-col sm:flex-row justify-center space-y-4 sm:space-y-0 sm:space-x-4">
           <button className="bg-orange-500 text-white px-6 py-3 rounded-lg text-lg font-medium hover:bg-orange-600 transition">
             Explore
@@ -35,6 +30,13 @@ function Home() {
             Join us
           </button>
         </div>
+        <h2 className="text-3xl font-semibold text-gray-800 mb-6 mt-6">
+          Why us?
+        </h2>
+        <p className="text-gray-600 mb-8 max-w-xl">
+          If you want to volunteer, host a campaign, or connect with others,
+          HelpHive is your perfect platform.
+        </p>
       </main>
       <footer className="mt-auto text-center text-gray-600 text-sm">
         <p>&copy; 2025 HelpHive. All rights reserved.</p>
