eb8ebee9ca74e69dbfc8af39fb570de2056fae51
Nayemja <154550902+Nayemja@users.noreply.github.com>
2025-02-07 15:27:08 +0600

    appjsx update
    app.jsx is updated for log in handle

---
+++ FrontEnd/help-hive/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About_US from "./pages/About-Us";
+import Home from "./pages/Home";
+import Explore from "./pages/Explore.jsx";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import ProfilePage from "./pages/profile_page.jsx";  
+// import Signup from "./pages/Signup";
+
+function App() {
+  const [user, setUser] = useState(null); // Track the user state (null means not logged in)
+
+  const handleLoginSuccess = (userData) => {
+    setUser(userData); 
+  };
+
+  const handleLogout = () => {
+    setUser(null); 
+  };
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/about" element={<About_US />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login onLoginSuccess={handleLoginSuccess} />} />
+          
+        
+          <Route 
+            path="/profile" 
+            element={user ? <ProfilePage user={user} onLogout={handleLogout} /> : <Login onLoginSuccess={handleLoginSuccess} />}
+          />
+
+          
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
