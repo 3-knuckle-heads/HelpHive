b8a8d9218402581a4fef1dd190627bce4e7d07bb
Nayemja <154550902+Nayemja@users.noreply.github.com>
2025-02-09 09:01:54 +0600

    api
    backend trying to work

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,56 +1,63 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
-const cors = require("cors");
-const openai = require("openai");
-const { default: OpenAI } = require("openai/index.mjs");
-const fs = require (fs);
+import cors from "cors";
+import openai from "openai";
+import { default as OpenAI } from "openai/index.mjs";
+import fs from "fs";
 
 const port = 4000;
 app.use(cors());
 
-app.get("/api/faq",async(req,res)=>
-{
+app.get("/api/faq", async (req, res) => {
   const question = req.query.question;
-  fs.readFile("server/company/feathures.txt","utf8",async(err,data)=>
-  {
-    if(err)
-      {
-        console.error("Error reading the file:",err);
-        return; 
-      }
-      if(question && question.length){
-       
-        const openai = new OpenAI({apiKey:OPENAI_API_KEY});
-        const ai_model = "gpt-3.5-turbo-1106";
+  fs.readFile("server/company/feathures.txt", "utf8", async (err, data) => {
+    if (err) {
+      console.error("Error reading the file:", err);
+      return;
+    }
+    if (question && question.length) {
+      const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
+      const ai_model = "gpt-3.5-turbo-1106";
+      // apiKey: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;
+      const secret =
+        sk -
+        proj -
+        IipSA8ecgO25uWgO0s1FfZGPrDzVCytZalMydAD -
+        y5HiqzH0zY4 -
+        ItXcDfSnWUbkYO5EpBDyIUT3BlbkFJar_ -
+        CQ6UsQtK6x4IUvKrwcYFDojiikUF5mRc11E -
+        XdZX4FZuBxiWnVuXMw5wG -
+        b2hgkaIv3UgA;
 
-        const prompt=[];
-        prompt.push("you are a company's support specialist available to answer any question.your ques mustbe on company details nothing else");
-        prompt.push("politely decline if the question doesnot match the following");
-        prompt.push(data);
+      const prompt = [];
+      prompt.push(
+        "you are a company's support specialist available to answer any question.your ques mustbe on company details nothing else"
+      );
+      prompt.push(
+        "politely decline if the question doesnot match the following"
+      );
+      prompt.push(data);
 
-        const message = [
-          {
-            role : "system",
-            content : prompt.join(" "),
-
-          },
-          {
-            role : "user",
-            content : question,
-
-          },
-        ];
-        const completion = await openai.chat.completions.create({
-          model:ai_model,
-          messages:message
-        });
-        const airesponse = completion.choices[0].message.content
-        res.json({airesponse});
-
-      }else{
-        res.json ({message:"no question is provided"})
-      }
-  })
-})
-app.listen(port,()=>console.log(`server running on port ${port}`));
+      const message = [
+        {
+          role: "system",
+          content: prompt.join(" "),
+        },
+        {
+          role: "user",
+          content: question,
+        },
+      ];
+      const completion = await openai.chat.completions.create({
+        model: ai_model,
+        messages: message,
+      });
+      const airesponse = completion.choices[0].message.content;
+      res.json({ airesponse });
+    } else {
+      res.json({ message: "no question is provided" });
+    }
+  });
+});
+app.listen(port, () => console.log(`server running on port ${port}`));
